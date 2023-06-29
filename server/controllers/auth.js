dec2843a36d02b4f3be35df481c7333110d3bb82
Anshuman Kundu <kundu4coding@gmail.com>
2023-06-29 12:49:15 +0530

    Backend Setup Done

---
+++ server/controllers/auth.js
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    console.log("control");
+    const { firstName, lastName, email, password } = req.body;
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+    await newUser.save();
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/auth", authRoutes);
+
+const PORT = process.env.PORT || 6001;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server Running on port", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
